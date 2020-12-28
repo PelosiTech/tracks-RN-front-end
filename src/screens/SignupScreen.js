476d2724b4fcd8d7700ac5d63dee320d9bbeb03e
Carlo Pelosi <cpelosi@asu.edu>
2020-12-27 21:44:47 -0700

    add ResolveAuthScreen, to show empty screen until we can see signed in

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect} from 'react';
+import React, { useContext} from 'react';
 import { View, StyleSheet } from "react-native";
 import {Context as AuthContext} from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
@@ -6,11 +6,8 @@ import NavLink from "../components/NavLink";
 import {NavigationEvents} from "react-navigation";
 
 const SignupScreen = () => {
-    const {state, signup, clearErrorMessage, tryLocalSignin} = useContext(AuthContext);
+    const {state, signup, clearErrorMessage} = useContext(AuthContext);
 
-    useEffect(() => {
-        tryLocalSignin();
-    }, [])
 
     return (
         <View style={styles.container}>
