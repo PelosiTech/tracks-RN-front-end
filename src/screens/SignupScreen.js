4354f84e0600143d4d0b6c9468be3494d38d6a43
Carlo Pelosi <cpelosi@asu.edu>
2020-12-27 18:35:38 -0700

    Start default mock up for navigation and build front end

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button} from 'react-native-elements'
+
+const SignupScreen = ({navigation}) => {
+    return (
+        <>
+        <Text h3>Sign Up For Tracker</Text>
+            <Input label='Email'/>
+            <Input label='Password'/>
+            <Button title='Sign up'/>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({})
+
+export default SignupScreen;
