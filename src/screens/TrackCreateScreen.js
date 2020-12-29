7d439306c5b2f939fe35d7ec329805aa9facf7f8
Carlo Pelosi <cpelosi@asu.edu>
2020-12-29 15:16:21 -0700

    add our real location to Map.js reactnativemaps

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
-// import './_mockLocation';
-import React, { useState, useEffect } from 'react';
-import { View, StyleSheet} from "react-native";
+import './_mockLocation';
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet} from "react-native";
 import {Text} from 'react-native-elements'
 import Map from "../components/Map";
 import {SafeAreaView} from "react-native-safe-area-context";
 import { requestPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
+import {Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+    const {addLocation} = useContext(LocationContext)
     const [err, setErr] = useState(null)
 
     const startWatching = async () => {
@@ -20,7 +22,7 @@ const TrackCreateScreen = () => {
                 timeInterval: 1000,
                 distanceInterval: 10
             }, (location) => {
-                console.log(location)
+                addLocation(location);
             })
         } catch (e) {
             setErr(e);
