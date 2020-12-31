cea2c52f71e8d636b026e632cd4b3c7f3f6fe516
Carlo Pelosi <cpelosi@asu.edu>
2020-12-30 19:17:02 -0700

    add icons to our bottom tab bar and fix header

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -8,6 +8,7 @@ import {SafeAreaView} from "react-native-safe-area-context";
 import {Context as LocationContext } from '../context/LocationContext';
 import useLocation from "../hooks/useLocation";
 import TrackForm from "../components/TrackForm";
+import { FontAwesome } from '@expo/vector-icons';
 
 
 const TrackCreateScreen = ({ isFocused }) => {
@@ -23,6 +24,11 @@ const TrackCreateScreen = ({ isFocused }) => {
         </SafeAreaView>
     );
 };
+
+TrackCreateScreen.navigationOptions = {
+    title: 'Add Track',
+    tabBarIcon: <FontAwesome name="plus" size={20} />,
+}
 
 const styles = StyleSheet.create({})
 
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -10,9 +10,6 @@ const TrackListScreen = ({navigation}) => {
     return (
         <>
             <NavigationEvents onWillFocus={fetchTracks} />
-            <Text style={{fontSize: 48}}>
-                TrackListScreen
-            </Text>
             <FlatList
                 data={state}
                 keyExtractor={item => item._id}
@@ -34,6 +31,10 @@ const TrackListScreen = ({navigation}) => {
         </>
     );
 };
+
+TrackListScreen.navigationOptions = {
+    title: 'Tracks'
+}
 
 const styles = StyleSheet.create({})
 
